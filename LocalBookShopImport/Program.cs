f5d531eee784736e53e87b7ea0985b35bace9ac4
Andre Plueth <andre.plueth@fh-muenster.de>
2017-11-10 19:49:55 +0100

    First try to crawl data fron openlibrary

--- LocalBookShopImport/Program.cs
+++ LocalBookShopImport/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace LocalBookShopImport
+﻿using System;
+using System.Collections.Generic;
+ using System.Data;
+
+namespace LocalBookShopImport
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            OpenLibraryBooksAPI api = new OpenLibraryBooksAPI();
+            DataTable dttemp = api.SearchBook("0072435097").Result;
+            Console.Write(dttemp.ToString());
             var book = Database.Create(Table.Book);
             book["title"] = "test! ;)";
 
