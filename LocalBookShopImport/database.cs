06846d2620249a2e971d37dee8e6b3c33d430b7d
Alexander Dölling <alexander.doelling@students.berufsakademie-oldenburg.de>
2017-11-10 15:44:14 +0100

    database connection

--- LocalBookShopImport/database.cs
+++ LocalBookShopImport/database.cs
@@ -5,6 +5,8 @@ namespace LocalBookShopImport
 {
     public class database
     {
+        NpgsqlConnection conn = new NpgsqlConnection("Server=172.16.2.200;User Id=lcal_boot_shop; " + 
+                                                     "Password=book123;Database=local_boot_shop;");
         
     }
 }
