685911143eb93c8603926dab50d487e7f9d8fddc
Alexander Dölling <alexander.doelling@students.berufsakademie-oldenburg.de>
2017-11-10 16:36:12 +0100

    database connection

--- LocalBookShopImport/Program.cs
+++ LocalBookShopImport/Program.cs
@@ -7,7 +7,15 @@ namespace LocalBookShopImport
     {
         public static void Main(string[] args)
         {
-            
+            database database = new database();
+            if (database.connTest())
+            {
+                Console.WriteLine("DB steht!");   
+            }
+            else
+            {
+                Console.WriteLine("DB geht nicht!"); 
+            }
         }
     }
 }
