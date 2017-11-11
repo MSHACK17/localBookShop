651007ae7c8468258fb5609cfd6d8f90508a0036
Kevin Haase <hak@viadee.de>
2017-11-11 15:27:06 +0100

    google api

--- localBookshopApp/src/components/book-detail/book-detail.ts
+++ localBookshopApp/src/components/book-detail/book-detail.ts
@@ -1,6 +1,7 @@
 import {Component,  Input} from '@angular/core';
 import {Book} from "../../models/book";
 import {Shop} from "../../models/shop";
+import {HttpClient} from "@angular/common/http";
 
 /**
  * Generated class for the BookDetailComponent component.
@@ -16,6 +17,10 @@ export class BookDetailComponent {
 
   title: string = 'Google Maps Addeed Successfully';
 
+
+
+  key:string = '&key=AIzaSyD29ws1Qewk5R_SQgkYRg3_qw8wLiRPWN4';
+
   lat: number = 51.950139;
 
   lng: number = 7.638549;
@@ -23,7 +28,7 @@ export class BookDetailComponent {
   strokeColor: '#FF0000';
   fillColor: '#FF0000';
 
-
+  url:string = 'https://maps.googleapis.com/maps/api/distancematrix/json?origins=' + this.lat+','+ this.lng+ '&destinations=';
 
 
 
@@ -33,26 +38,52 @@ export class BookDetailComponent {
   shopObjects:any;
 
 
-  constructor() {
+  constructor(private http:HttpClient) {
   }
   ngAfterContentInit(){
     console.info(this.book);
     this.shopObjects = [];
 
-    this.shops.forEach((shop)=>{
-      console.log(shop);
-      console.log(JSON.parse(shop.position));
-      this.shopObjects.push({
-        id:  shop.id,
-        name: shop. name,
-        city: shop.city,
-        zip: shop.zip,
-        street: shop.street,
-        position: shop.position != null ? JSON.parse(shop.position) : null,
-        amount: shop.amount
-      })
-    })
+    this.shops.forEach((shop, position)=> {
+      let positionObject = JSON.parse(shop.position);
+      if(position==0){
+        this.url +=positionObject.lat +","+ positionObject.long;
+      }else{
+        this.url +='|' + positionObject.lat +","+ positionObject.long;
+      }
+    });
 
+    this.url +=this.key;
+    this.http.get(this.url).subscribe((data) =>{
+      console.log(data);
+      this.shops.forEach((shop,position)=>{
+
+        this.shopObjects.push({
+          id:  shop.id,
+          name: shop. name,
+          duration: data['rows'][0].elements[position].duration.text,
+          durationSort: data['rows'][0].elements[position].distance.value,
+          distance: data['rows'][0].elements[position].distance.text,
+          city: shop.city,
+          zip: shop.zip,
+          street: shop.street,
+          position: shop.position != null ? JSON.parse(shop.position) : null,
+          amount: shop.amount
+        })
+      })
+    });
+
+    this.shopObjects.sort(function (a, b) {
+      if (a.durationSort > b.durationSort) {
+        return 1;
+      }
+      if (a.durationSort < b.durationSort) {
+        return -1;
+      }
+      // a muss gleich b sein
+      return 0;
+    });
+  console.log(this.shopObjects);
   }
   showShopDetail(shop:any){
     console.info(shop);
