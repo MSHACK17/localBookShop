d24476c0afb25d48c22c6be884193cd1cb5a4c39
Philip Weweler <philip.weweler@fh-muenster.de>
2017-11-11 10:08:22 +0100

    added Genre and Publisher Model

---
+++ LocalBookShopImport/Model/Genre.cs
@@ -0,0 +1,18 @@
+﻿using LimeBean;
+
+namespace LocalBookShopImport.Model
+{
+    public class Genre : Bean
+    {
+        public Genre() : base("genre") { }
+        
+        public int Id => Get<int>("id");
+
+        public string Name
+        {
+            get { return Get<string>("name"); }
+            set { Put("name", value); }
+        }
+
+    }
+}
